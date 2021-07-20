db0721e5022caf05904cea5211bc7d969a3feda5
adrianlafo <adrianlafontaine2@gmail.com>
2021-07-20 14:24:54 -0500

    Second Commit

---
+++ src/components/WeatherInputs.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function WeatherInputs() {
+	return (
+		<div className="container">
+			<div className="">
+				<input type="text" placeholder="Enter your city..." />
+			</div>
+		</div>
+	)
+}
+
+export default WeatherInputs
